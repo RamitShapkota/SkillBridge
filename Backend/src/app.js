4860f3069164348c8a8b7a88b8e17074a8929cb6
RamitShapkota <shapkotaramit47@gmail.com>
2026-06-08 09:49:59 +0545

    Implement global error handler middleware and integrate it into the app

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { globalErrorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -15,5 +16,7 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.use(globalErrorHandler);
 
 export { app };
