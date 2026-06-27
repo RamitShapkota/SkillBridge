935b24a02a584240544599598c68fdc42e767638
RamitShapkota <shapkotaramit47@gmail.com>
2026-06-27 13:20:30 +0545

    feat(models): add job model with schema definitions

---
+++ Backend/src/models/job.model.js
@@ -0,0 +1,83 @@
+import mongoose from "mongoose";
+
+const jobSchema = new mongoose.Schema(
+  {
+    client: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    requirements: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    skills: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+
+    budget: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+
+    duration: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    deadline: {
+      type: Date,
+      required: true,
+    },
+
+    complexity: {
+      type: String,
+      enum: ["beginner", "intermediate"],
+      required: true,
+    },
+
+    attachments: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+
+    status: {
+      type: String,
+      enum: ["open", "closed"],
+      default: "open",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Job = mongoose.model("Job", jobSchema);
